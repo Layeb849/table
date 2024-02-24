1be2cbbc566b459bcc3c17d796986f88dde11610
Layeb849 <layebhossain849@gmail.com>
2024-02-24 22:22:45 +0600

    table project

---
+++ base.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+    <table>
+    <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>
+                    <label for="">product name:</label>
+                    <input type="text" name="alu">
+                    </td>
+                    <td>
+                    <label for="">product price:</label>
+                    <input type="number" name="50">
+                </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>
+                    <label for="">product name:</label>
+                    <input type="text" name="moris">
+                    </td>
+                    <td>
+                    <label for="">product price:</label>
+                    <input type="number" name="100">
+                </td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>
+                    <label for="">product name:</label>
+                    <input type="text" name="begun">
+                    </td>
+                    <td>
+                    <label for="">product price:</label>
+                    <input type="number" name="40">
+                </td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>
+                    <label for="">product name:</label>
+                    <input type="text" name="potol">
+                    </td>
+                    <td>
+                    <label for="">product price:</label>
+                    <input type="number" name="20">
+                </td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>
+                    <label for="">product name:</label>
+                    <input type="text" name="kumra">
+                    </td>
+                    <td>
+                    <label for="">product price:</label>
+                    <input type="number" name="30">
+                </td>
+                </tr>
+               
+            </tbody>
+    </table>
+    <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <table>
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Description</th>
+                    <th>price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>
+                    <?php echo 'alu' ?>
+                    </td>
+                    <td>
+                    <?php echo '50'  ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><?php echo 'moris' ?></td>
+                    <td><?php echo '100' ?></td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><?php echo 'begun' ?></td>
+                    <td><?php echo '40' ?></td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><?php echo 'potol' ?></td>
+                    <td><?php echo '20' ?></td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><?php echo 'kumra' ?></td>
+                    <td><?php echo '30' ?></td>
+                </tr>
+                
+            </tbody>
+            <tfoot>
+            <td> </td>
+            <td>total price = </td>
+            <td>total</td>
+            </tfoot>
+        </table> 
+</body>
+</html>
